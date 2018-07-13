8461da3a15aa59374144eb7772f81fbf96df7cc6
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-07-13 17:21:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ let currentUser="Louis"
 
 const welcomeMessage= "Welcome to Flatbook, "+ currentUser +"!"
 
-const excitedWelcomeMessage ="WELCOME TO FLATBOOK, " + currentUser.capitalize +"!"
+const excitedWelcomeMessage ="WELCOME TO FLATBOOK, " + currentUser +"!"
   
